611129da824cf96b8c70be3c95080606deacf01b
Brenda Wallace <shiny@cpan.org>
2015-04-23 11:51:19 +1200

    added fixtures for prompts

--- app/models/prompt.js
+++ app/models/prompt.js
@@ -1,9 +1,16 @@
 import DS from 'ember-data';
 
 var Prompt = DS.Model.extend({
+  content: DS.attr('string'),
   blanks: DS.hasMany('blank'),  //e.g. we collect info about your _blank_
   response: DS.attr('string'), //what going in the blank, e.g. "location"
-  stepNumber: DS.attr('integer') 
+  step: DS.attr('integer') 
+});
+
+Prompt.reopenClass({
+  FIXTURES: [
+  {'id': 1, 'step': 1, 'content': 'My business is called'}
+  ]
 });
 
 export default Prompt;
