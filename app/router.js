56e397706600705b0e7cc356c7deda94a050a10e
Brenda Wallace <shiny@cpan.org>
2015-04-29 11:19:35 +1200

    tabulation fix up

--- app/router.js
+++ app/router.js
@@ -6,11 +6,8 @@ var Router = Ember.Router.extend({
 });
 
 export default Router.map(function() {
-
   this.resource('step', {path: 'step/:step'}, function () {
-      this.route('progress');    
+    this.route('progress');    
   });
-
   this.route('whatsThis', {path: 'whatsThis'});
-  
 });
