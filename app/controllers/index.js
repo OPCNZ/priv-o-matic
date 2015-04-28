fe3864b6983fc99a4943526ad760210ea2aa2b66
Brenda Wallace <shiny@cpan.org>
2015-04-29 11:18:07 +1200

    moved transitions from controller to route

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,12 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  actions: {
-    getStarted: function () {
-      this.transitionTo('step', 1);
-    },
-    whatsThis: function() {
-      this.transitionTo('whatsThis');
-    }
-  }
+
 });
