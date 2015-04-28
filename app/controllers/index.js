fc46ea37ffd81c3ecf570522ddb4a3ff85dc3a6e
Brenda Wallace <shiny@cpan.org>
2015-04-29 10:27:40 +1200

    fixed transition

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
 	actions: {
 		getStarted: function () {
-			this.transitionTo('step/1');
+			this.transitionTo('step', 1);
 		},
 		whatsThis: function() {
 			this.transitionTo('whatsThis');
