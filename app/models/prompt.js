f22559cd1e3f0fe41867e201c2d3fbbc80118709
Brenda Wallace <shiny@cpan.org>
2015-04-29 17:07:23 +1200

    tabulation fix

--- app/models/prompt.js
+++ app/models/prompt.js
@@ -9,29 +9,29 @@ let Prompt = DS.Model.extend({
 });
 
 let data = [
-	{step: 1, prompts: ['My business is called %businessName'] },
-	{step: 2, prompts: ['How will the weather be tomorrow? %weather'] },
-	{step: 3, prompts: ['Do you use google adsense? %goodle-adsense', 'Do you use Amazon Web Services? %amazon'] },
-	{step: 4, prompts: ['Where do you store the data collected? %data-collected']}
-	];
+  {step: 1, prompts: ['My business is called %businessName'] },
+  {step: 2, prompts: ['How will the weather be tomorrow? %weather'] },
+  {step: 3, prompts: ['Do you use google adsense? %goodle-adsense', 'Do you use Amazon Web Services? %amazon'] },
+  {step: 4, prompts: ['Where do you store the data collected? %data-collected']}
+  ];
 
 //Turn the data above into the format that ember fixtures wants
 var fixtures = [];
 var id_number = 0;
 
 for (let step of data) {
-	id_number +=1;
-	let step_number = step.step;
-	let prompts = step.prompts;
-	for (let question of prompts) {
-		fixtures.push({id: id_number, question: question, step: step_number});
-	}
+  id_number +=1;
+  let step_number = step.step;
+  let prompts = step.prompts;
+  for (let question of prompts) {
+    fixtures.push({id: id_number, question: question, step: step_number});
+  }
 }
 
 console.log(fixtures);
 
 Prompt.reopenClass({
-	FIXTURES: fixtures
+  FIXTURES: fixtures
 });
 
 export default Prompt;
