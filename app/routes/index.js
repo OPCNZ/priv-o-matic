7c7481fa311726b6bd7c204950f26d2c16d4ed89
Brenda Wallace <shiny@cpan.org>
2015-04-29 17:04:25 +1200

    removed unused actions

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,12 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  actions: {
-    getStarted: function () {
-      this.transitionTo('step', 1);
-    },
-    whatsThis: function() {
-      this.transitionTo('whatsThis');
-    }
-  }  
 });
