5732f316d0954ad7297b3ec1a0d19f7e64c41353
Brenda Wallace <shiny@cpan.org>
2015-04-29 16:48:16 +1200

    tabulation cleanup

--- app/controllers/step.js
+++ app/controllers/step.js
@@ -1,12 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-	actions: {
-		nextStep: function () {
-			console.log("Next step");
-		}
-	},
-	prompts: function () {
-		this.model.get('prompts');
-	}.property('@each.prompts'),
+  actions: {
+    nextStep: function () {
+      console.log("Next step");
+    }
+  },
+  prompts: function () {
+    this.model.get('prompts');
+  }.property('@each.prompts'),
 });
