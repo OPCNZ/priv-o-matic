a239e0d9ff7dbc2d9ab1981aad2cbbeefd7c1bf7
Brenda Wallace <shiny@cpan.org>
2015-04-29 15:42:43 +1200

    add steps in router

--- app/router.js
+++ app/router.js
@@ -6,8 +6,8 @@ var Router = Ember.Router.extend({
 });
 
 export default Router.map(function() {
-  this.resource('step', {path: 'step/:step'}, function () {
-    this.route('progress');    
+  this.resource('steps', function () {
+    this.route('step', {path: 'step/:step_id'});
   });
   this.route('whatsThis', {path: 'whatsThis'});
 });
