17778f53fd4632067a15d5bcdba1ea1b8185ba2e
Brenda Wallace <brenda@rabidtech.co.nz>
2015-05-06 11:40:45 +1200

    other toggles on and off

--- app/components/step-data-types.js
+++ app/components/step-data-types.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  needs: ['steps'],
+  actions: {
+
+  },
+
+  otherData: undefined,
+  watchOtherdata: function() {
+    this.toggleProperty('isShowingOther');
+  }.observes('otherData')
+
 });
