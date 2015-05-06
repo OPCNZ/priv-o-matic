c906b299c7861d88a14169a9923d8a686f76563f
Brenda Wallace <brenda@brenda.nz>
2015-05-07 11:01:47 +1200

    identity data and model saving

--- app/components/step-data-types.js
+++ app/components/step-data-types.js
@@ -1,14 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  needs: ['steps'],
-  actions: {
+    needs: ['steps'],
+    actions: {
+        nextStep: function () {
+      		this.model.save();
+            
+            let next_step = parseInt(this.model.id) + 1;
+            this.transitionToRoute('steps.step', next_step);  		
+      	}
+    },
 
-  },
+    otherData: undefined,
+    watchOtherdata: function() {
+  	   this.model.otherData = true;
+       //show the text field for entering other
+       this.toggleProperty('isShowingOther');
+    }.observes('otherData'),
 
-  otherData: undefined,
-  watchOtherdata: function() {
-    this.toggleProperty('isShowingOther');
-  }.observes('otherData')
-
+    identityData: undefined,
+    watchidentityData: function () {
+  	   this.model.identityData = !!this.get('identityData');
+    }.observes('identityData')
 });
--- app/controllers/steps/step.js
+++ app/controllers/steps/step.js
@@ -2,11 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   actions: {
-    nextStep: function () {
-      console.log("Next step");
-      let next_step = parseInt(this.model.id) + 1;
-      this.transitionToRoute('steps.step', next_step);
-    }
   }
 
 });
