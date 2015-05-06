6dd07876c66f17b6e3df472ff8672bbb8ad69cba
Brenda Wallace <brenda@brenda.nz>
2015-05-07 11:28:29 +1200

    changed our hard coded step count

--- app/controllers/steps/step.js
+++ app/controllers/steps/step.js
@@ -3,11 +3,12 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
     actions: {
         nextStep: function () {
+            this.model.set('isCompleted', true);
             this.model.save();
 
             let next_step = parseInt(this.model.id) + 1;
 
-            if (next_step < 4) { //TODO work out how many steps there are
+            if (next_step < 9) { //TODO work out how many steps there are
                 this.transitionToRoute('steps.step', next_step);          
             }
             else {
