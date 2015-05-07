e2beac381d4aac0271f21e03fc5d11e757132b3c
Brenda Wallace <brenda@rabidtech.co.nz>
2015-05-08 09:20:32 +1200

    removed unneeded variables
    Conflicts:
    	app/components/step-data-types.js

--- app/components/step-data-types.js
+++ app/components/step-data-types.js
@@ -1,14 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  needs: ['steps'],
-  actions: {
-
-  },
-
-  otherData: undefined,
   watchOtherdata: function() {
     this.toggleProperty('isShowingOther');
-  }.observes('otherData')
-
+  }.observes('model.otherData'),
 });
