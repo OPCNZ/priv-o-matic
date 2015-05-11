7c88bf1d16cf6b6bdc48f4392819871a2935a814
Brenda Wallace <brenda@rabidtech.co.nz>
2015-05-11 12:03:55 +1200

    used mapped properties to display the otherData field

--- app/components/step-data-types.js
+++ app/components/step-data-types.js
@@ -1,7 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  watchOtherdata: function() {
-    this.toggleProperty('isShowingOther');
-  }.observes('model.otherData'),
+
 });
