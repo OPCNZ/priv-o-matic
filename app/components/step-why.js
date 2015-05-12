498e17ced17ef11d15a11860428680fa160a6caa
Brenda Wallace <brenda@rabidtech.co.nz>
2015-05-12 16:40:07 +1200

    allow extra reasons why we collect data

--- app/components/step-why.js
+++ app/components/step-why.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    addReason: function () {
+      var reasons = this.get('model.statement.extraReasons');
+      if (! reasons) {
+        this.set('model.statement.extraReasons', Ember.A(['one']));
+      }
+      else {
+        reasons.pushObject('more');
+      }
+    }
+  },
+
+
 });
