d250df39c5db72b7fc918c19d8f17c8c06e77ef8
Brenda Wallace <brenda@rabidtech.co.nz>
2015-05-12 16:39:42 +1200

    removed helpful but verbose coments

--- app/components/step-sharing.js
+++ app/components/step-sharing.js
@@ -5,17 +5,9 @@ export default Ember.Component.extend({
     addParty: function () {
       var parties = this.get('model.statement.sharingParties');
       if (! parties) {
-        // Ember.A will create an "Ember Array" which is a superset of the
-        // normal JS Array type except it it key-value observing aware
-        //
-        // TLDR: If you are creating an array literal wrap it in Ember.A like this
         this.set('model.statement.sharingParties', Ember.A(['one']));
       }
       else {
-        // #pushObject is a method on Ember.Array that does the same as #push
-        // except it also causes ember to update anything observing the array
-        //
-        // TLDR: use #pushObject not #push to add values to an ember array
         parties.pushObject('more');
       }
     }
