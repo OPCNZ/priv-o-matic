1ae8376ddc4fee76640d1c8996b4cea53fd0cd2c
Brenda Wallace <brenda@rabidtech.co.nz>
2015-05-12 15:00:53 +1200

    sharing reasons - re-rending template not working

--- app/components/step-sharing.js
+++ app/components/step-sharing.js
@@ -1,4 +1,26 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    addParty: function () {
+      var statement = this.model.get('statement');
+      var parties = statement.get('sharingParties');
+      if (! parties) {
+        parties = ['one'];
+      }
+      else {
+        parties.push('more');
+      }
+
+      statement.set('sharingParties', parties);
+
+      console.log(parties);
+      debugger;
+      
+    }
+  },
+
+  sharingParties: function () {
+    return this.model.get('statement.sharingParties')
+  }.property('@each.model.statement.sharingParties')
 });
