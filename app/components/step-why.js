2382067cd6c3d9e1b154d1809f031b1b01b8f0d3
Brenda Wallace <brenda@rabidtech.co.nz>
2015-05-14 09:09:29 +1200

    we don't need to initialise an array into statement.extraReasons anymore.

--- app/components/step-why.js
+++ app/components/step-why.js
@@ -4,12 +4,8 @@ export default Ember.Component.extend({
   actions: {
     addReason: function () {
       var reasons = this.get('model.statement.extraReasons');
-      if (! reasons) {
-        this.set('model.statement.extraReasons', Ember.A(['']));
-      }
-      else {
-        reasons.pushObject('');
-      }
+      reasons.pushObject('');
+    },
     }
   },
 
