535e246aa0358f9b64bfdb16a3ed9f33a2be182d
Brenda Wallace <brenda@brenda.nz>
2015-05-14 17:43:26 +1200

    extra reasons always an array

--- app/components/step-why.js
+++ app/components/step-why.js
@@ -4,14 +4,7 @@ export default Ember.Component.extend({
   actions: {
     addReason: function () {
       var reasons = this.get('model.statement.extraReasons');
-      if (! reasons) {
-        this.set('model.statement.extraReasons', Ember.A(['one']));
-      }
-      else {
-        reasons.pushObject('more');
-      }
-    }
+      reasons.pushObject('');
+    },
   },
-
-
 });
