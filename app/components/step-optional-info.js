95816454e096af3d8b6b09663a10a3e9f6de10f1
Brenda Wallace <brenda@brenda.nz>
2015-05-18 16:04:27 +1200

    toggle help, ember-ised.

--- app/components/step-optional-info.js
+++ app/components/step-optional-info.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    actions: {  
+        toggleHelp: function () {
+            this.toggleProperty('showHelp');
+        }
+    }
 });
