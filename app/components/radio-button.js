82aa10c10db42d6de95c19201fed1b420f1e0b31
Brenda Wallace <brenda@wallace.net.nz>
2015-05-19 09:47:30 +1200

    Use a radio button.

---
+++ app/components/radio-button.js
@@ -0,0 +1,5 @@
+import EmberRadioButton from 'ember-radio-buttons';
+
+export default EmberRadioButton;
+
+
--- app/components/step-why.js
+++ app/components/step-why.js
@@ -17,35 +17,6 @@ export default Ember.Component.extend({
       this.set('notReadyModal', false);
     }
   },
-
-  setToYes: function () {
-    this.model.set('statement.onlyTheseReasons', true);
-    this.model.set('statement.notOnlyTheseReasons', false);
-  },
-  setToNo: function () {
-    this.model.set('statement.onlyTheseReasons', false);
-    this.model.set('statement.notOnlyTheseReasons', true);
-  },
-
-  //make a adhoc radio button like behaviour on our 2 checkboxes
-  onlyTheseReasons: function () {
-      if(!! this.get('model.statement.onlyTheseReasons')) {
-        this.setToYes();
-      }
-      else {
-       this.setToNo();
-      }
-  }.observes('model.statement.onlyTheseReasons'),
-
-  notOnlyTheseReasons: function () {
-      if(!! this.get('model.statement.notOnlyTheseReasons')) {
-        this.setToNo();
-      }
-      else {
-       this.setToYes();
-      }
-  }.observes('model.statement.notOnlyTheseReasons'),
-
   readyToMoveOn: function () {
 
     //only these reasons
