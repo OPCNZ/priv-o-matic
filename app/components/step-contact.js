adb467164fd9cc38da9f786f0409ee87208f9047
Brenda Wallace <brenda@wallace.net.nz>
2015-05-19 16:49:29 +1200

    use the stepHelp component

--- app/components/step-contact.js
+++ app/components/step-contact.js
@@ -1,9 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    actions: {
-        toggleHelp: function () {
-            this.toggleProperty('showHelp');
-        }
-    }
 });
