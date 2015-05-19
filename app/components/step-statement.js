6f9468ee0c615a5f48b1835cc9125e58f05a97c7
Brenda Wallace <brenda@rabidtech.co.nz>
2015-05-19 20:10:51 +1200

    dataDestruction check box after they enter the details

--- app/components/step-statement.js
+++ app/components/step-statement.js
@@ -4,5 +4,9 @@ export default Ember.Component.extend({
   keptSecurely: function (){
     //have they filled in the secure method?
     return !! (this.model.get('statement.secureMethodOne'));
-  }.property('model.statement.secureMethodOne')
+  }.property('model.statement.secureMethodOne'),
+  dataDestruction: function (){
+    //have they filled in the secure method?
+    return !! (this.model.get('statement.dataRetentionTime'));
+  }.property('model.statement.dataRetentionTime')
 });
