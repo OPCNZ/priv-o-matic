6a221f4c790106dc666010ff73c528077d21f9b3
Brenda Wallace <brenda@wallace.net.nz>
2015-05-19 18:05:05 +1200

    Checks if the form is ready to "move along" and passes the action to the contoller if it is

--- app/components/step-data-types.js
+++ app/components/step-data-types.js
@@ -4,6 +4,29 @@ export default Ember.Component.extend({
   actions: {
     acknowledgeNoDataModal: function () {
       this.model.set('statement.noData', false);
+      this.set('showNotReadyMessage', false);
+    },
+
+    nextStep: function () {
+      if (this.isReady()) {
+        this.sendAction();
+      }
+      else {
+        this.set('showNotReadyMessage', true);
+      }
     }
+  },
+  isReady: function () {
+
+    //if any of this array is true, then we're ready to move forward
+    var options = ['identityData', 'locationData', 'userBehaviourData', 'computerNetworkData', 'financialData', 'otherData'];
+    for (var i = options.length - 1; i >= 0; i--) {
+      if (this.get('model.statement.' + options[i])) {
+        return true;
+      }
+    }
+    return false;
   }
+
+
 });
