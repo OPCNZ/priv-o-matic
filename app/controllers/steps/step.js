c481f481e4ededb252d01ddc1e185b66561c953e
Brenda Wallace <brenda@wallace.net.nz>
2015-05-19 14:52:46 +1200

    Allow some step models to lead back to step 8.

--- app/controllers/steps/step.js
+++ app/controllers/steps/step.js
@@ -6,15 +6,21 @@ export default Ember.Controller.extend({
             this.model.set('isCompleted', true);
             this.model.save();
 
-            let next_step = parseInt(this.model.id, 10) + 1;
+            //try to read next step from the model.
+            let nextStep = this.model.get('nextStep');
 
-            if (next_step < 9) { //TODO work out how many steps there are
-                this.transitionToRoute('steps.step', next_step);          
+            //nothing on the model, so increment to find the next step
+            if (!nextStep) {
+                nextStep = parseInt(this.model.id, 10) + 1;
+            }
+
+            if (nextStep < 9) { //TODO work out how many steps there are
+                this.transitionToRoute('steps.step', nextStep);
             }
             else {
                 this.transitionToRoute('steps.finish');
             }
-        }    
+        }
     }
 
 });
