7e8ca6035490e96b5e3a0b55e4651aa0fe1c6741
Brenda Wallace <brenda@wallace.net.nz>
2015-05-19 15:15:41 +1200

    modal dialouge when info is required by law

--- app/components/step-legal-requirement.js
+++ app/components/step-legal-requirement.js
@@ -1,9 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    actions: {  
+    actions: {
         toggleHelp: function () {
             this.toggleProperty('showHelp');
+        },
+        showModal: function() {
+            this.set('showModal', true);
+        },
+        ackModal: function() {
+            this.set('showModal', false);
         }
-    }    
+
+    }
 });
