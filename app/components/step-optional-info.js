614683aefaa60b2ea242ee7ed7e9b54870a6064e
Brenda Wallace <brenda@rabidtech.co.nz>
2015-05-19 19:20:53 +1200

    action for adding yet more optional info

--- app/components/step-optional-info.js
+++ app/components/step-optional-info.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    addOptionalInfo: function () {
+      var optional = this.get('model.statement.extraOptionalInfo');
+      optional.pushObject(Ember.A({}));
+    }
+  },
 });
