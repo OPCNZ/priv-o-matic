5fa087f37d416dc5bd84d08f0427434a19837f8a
Brenda Wallace <brenda@wallace.net.nz>
2015-05-19 18:13:35 +1200

    more comments

--- app/components/step-data-types.js
+++ app/components/step-data-types.js
@@ -11,15 +11,20 @@ export default Ember.Component.extend({
     },
 
     nextStep: function () {
+      //check is we are ready to move forward
       if (this.isReady()) {
         this.sendAction();
       }
       else {
+        //we're not ready, so show the model message
         this.set('showNotReadyMessage', true);
       }
     }
   },
   isReady: function () {
+    /**
+    Check if our form is filled out
+    */
 
     //if any of this array is true, then we're ready to move forward
     var options = ['identityData', 'locationData', 'userBehaviourData', 'computerNetworkData', 'financialData', 'otherData'];
