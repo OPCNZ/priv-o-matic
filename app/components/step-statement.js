1961bf85bfeca0437120faa74ce04b3119bb8065
Brenda Wallace <brenda@wallace.net.nz>
2015-05-19 20:03:55 +1200

    the keptSecurely box is checked if they've entered how it's secured.

--- app/components/step-statement.js
+++ app/components/step-statement.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  keptSecurely: function (){
+    //have they filled in the secure method?
+    return !! (this.model.get('statement.secureMethodOne'));
+  }.property('model.statement.secureMethodOne')
 });
