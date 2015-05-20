e988a7972b98bf2247640ca244cf23f5d81b47f9
Brenda Wallace <brenda@wallace.net.nz>
2015-05-20 16:22:50 +1200

    checks entry on step-2. displays modal if not ready

--- app/components/step-data-sources.js
+++ app/components/step-data-sources.js
@@ -4,5 +4,32 @@ export default Ember.Component.extend({
   //These change to false after the modals have been seen once,
   //thereby not showing them again.
   showOtherDataModal: true,
-  showThirdPartyDataModal: true
+  showThirdPartyDataModal: true,
+
+  actions: {
+    nextStep: function () {
+      //check is we are ready to move forward
+      if (this.isReady()) {
+        this.sendAction();
+      }
+      else {
+        //we're not ready, so show the modal-message
+        this.set('showNotReadyMessage', true);
+      }
+    }
+  },
+  isReady: function () {
+    /**
+    Check if our form is filled out
+    */
+    //if any of this array is true, then we're ready to move forward
+    var options = ['userDataSource', 'publicDataSource', 'thirdPartyDataSource', 'otherDataSource'];
+    var self = this;
+
+    return options.any(function(option) {
+      if (self.get('model.statement.' + option)) {
+        return true;
+      }
+    });
+  }
 });
