82f4c2447fe56c6a1f2c140d10669eee3fe19df1
Brenda Wallace <brenda@wallace.net.nz>
2015-05-21 12:03:18 +1200

    move fixtures to initializers

--- app/models/step.js
+++ app/models/step.js
@@ -1,6 +1,6 @@
 import DS from 'ember-data';
 
-var Step = DS.Model.extend({
+export default DS.Model.extend({
   statement: DS.belongsTo('statement', {async: true}),
   title: DS.attr('string'),
   name: DS.attr('string'), //how we find our component
@@ -8,73 +8,4 @@ var Step = DS.Model.extend({
   nextStep: DS.attr(),
   showInNav: DS.attr('boolean', {defaultValue: true})
 });
-
-
-Step.reopenClass({
-  FIXTURES: [
-  {  id: 1,
-    statement: 1,
-    title: 'Information',
-    name: 'step-data-types'
-  },
-  {  id: 2,
-    statement: 1,
-    title: 'We get this data from',
-    name: 'step-data-sources'
-  },
-  {
-    id: 3,
-    statement: 1,
-    title: 'Why do you need that information?',
-    name: 'step-why'
-  },
-  {
-    id: 4,
-    statement: 1,
-    title: 'Who do you share with and why?',
-    name: 'step-sharing'
-  },
-  {
-    id: 5,
-    statement: 1,
-    title: 'Contact',
-    name: "step-contact"
-  },
-  {
-    id: 6,
-    statement: 1,
-    title: 'Optional Information',
-    name: "step-optional-info"
-  },
-  {
-    id: 7,
-    statement: 1,
-    title: 'Applicable Laws',
-    name: "step-legal-requirement"
-  },
-  {
-    id: 8,
-    statement: 1,
-    title: 'Privacy Statement',
-    name: "step-statement",
-  },
-  {
-    id: 9,
-    statement: 1,
-    title: 'Security',
-    name: "step-security",
-    nextStep: 8,
-    showInNav: false,
-  },
-  {
-    id: 10,
-    statement: 1,
-    title: 'Retention',
-    name: "step-retention",
-    nextStep: 8,
-    showInNav: false,
-  },
-  ]
-});
-
-export default Step;
+;
