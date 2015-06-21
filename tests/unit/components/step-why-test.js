14075804d66ffc71bff4e11623de4a33f57b10d9
Brenda Wallace <brenda@rabidtech.co.nz>
2015-06-22 09:48:12 +1200

    step-why needs radio-button

--- tests/unit/components/step-why-test.js
+++ tests/unit/components/step-why-test.js
@@ -5,7 +5,7 @@ import {
 
 moduleForComponent('step-why', {
   // Specify the other units that are required for this test
-  // needs: ['component:foo', 'helper:bar']
+  needs: ['component:radio-button']
 });
 
 test('it renders', function(assert) {
