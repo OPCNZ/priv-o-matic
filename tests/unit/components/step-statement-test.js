c3a9d8317284c618e84d76b396a49ed2373c7f79
Brenda Wallace <brenda@rabidtech.co.nz>
2015-06-23 10:07:17 +1200

    Added a fake model to step-statement component , in unit test.

--- tests/unit/components/step-statement-test.js
+++ tests/unit/components/step-statement-test.js
@@ -3,19 +3,37 @@ import {
   test
 } from 'ember-qunit';
 
+
 moduleForComponent('step-statement', {
   // Specify the other units that are required for this test
-  // needs: ['component:foo', 'helper:bar']
+  needs: [
+    'view:statement',
+    'component:step-help',
+    'model:statement',
+    'initializer:load-fixtures'
+  ]
 });
 
+import Ember from 'ember';
+
 test('it renders', function(assert) {
-  assert.expect(2);
+  assert.expect(3);
 
   // Creates the component instance
   var component = this.subject();
   assert.equal(component._state, 'preRender');
 
+  var Statement = Ember.Object.extend({
+  });
+
+  var statement = Statement.create({  id: 1 });
+  component.set('model', statement);
+
+  //the model was loaded
+  assert.ok(component.get('model'), "component has a model");
+
   // Renders the component to the page
   this.render();
   assert.equal(component._state, 'inDOM');
+
 });
